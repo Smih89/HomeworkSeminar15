03868d41b65b77d84c5d34efdd8f35d02a81fe05
SergeyMikheev <s.a.mikheev@yandex.ru>
2023-02-01 15:50:14 +0300

    apdate Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 64
+//Задайте значение N. Напишите программу, которая выведет 
+//все натуральные числа в промежутке от N до 1.
+//Выполнить с помощью рекурсии.
+
+void MyTask1(int n)
+{
+  if (n == 0)
+    return;
+
+  if (n == 1)
+    Console.Write($"{n}");
+  else
+    Console.Write($"{n}, ");
+
+  MyTask1(n - 1);
+}
+Console.WriteLine("Введите n:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"N = {n} -> ");
+MyTask1(n);
+
+Console.ReadLine();
