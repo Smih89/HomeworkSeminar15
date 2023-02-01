e5f75db92672b671198f7e9d41b610a1fba14ce6
SergeyMikheev <s.a.mikheev@yandex.ru>
2023-02-01 16:16:32 +0300

    apdate Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 68
+//Напишите программу вычисления функции Аккермана с помощью рекурсии.
+//Даны два неотрицательных числа m и n.
+
+int A(int n, int m)
+{
+  if (n == 0) return m + 1;
+  if (n != 0 && m == 0) return A(n - 1, 1);
+  if (n > 0 && m > 0) return A(n - 1, A(n, m - 1));
+  return A(n, m);
+}
+Console.WriteLine("Введите m:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите n:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"M = {m}, N = {n} -> {A(m, n)}");
+
+Console.ReadLine();
